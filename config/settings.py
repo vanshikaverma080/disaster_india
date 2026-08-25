7a7a93bfd825dde012272b340303bfd8dd69c95e
Vanshika <vvanshika135@gmail.com>
2026-08-25 13:46:53 +0000

    production style changes

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ DATABASES={"default":{"ENGINE":"django.db.backends.sqlite3","NAME":BASE_DIR/"cli
 AUTH_PASSWORD_VALIDATORS=[]
 LANGUAGE_CODE="en-us"; TIME_ZONE="Asia/Kolkata"; USE_I18N=True; USE_TZ=True
 STATIC_URL="/static/"; STATICFILES_DIRS=[BASE_DIR/"static"]
+STATIC_ROOT=BASE_DIR/"staticfiles"
+
 DEFAULT_AUTO_FIELD="django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS=os.getenv("CORS_ALLOW_ALL_ORIGINS","False").lower()=="true"
 CORS_ALLOWED_ORIGINS=[x.strip() for x in os.getenv("CORS_ALLOWED_ORIGINS","http://127.0.0.1:8000,http://localhost:8000").split(",") if x.strip()]
--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,17 @@
+
 from django.contrib import admin
 from django.urls import path, include
-urlpatterns=[
- path("admin/",admin.site.urls),
- path("api/",include("core.urls")),
- path("",include("core.frontend_urls")),
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("core.urls")),
+    path("", include("core.frontend_urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.STATIC_URL,
+        document_root=settings.STATIC_ROOT
+    )
